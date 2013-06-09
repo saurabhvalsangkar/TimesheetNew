8fd23050f9a7b7d4fd53e6c6fb075ef05330ab9a
saurabhvalsangkar <admin@LTDLUK01445.cts.com>
2013-06-10 00:46:28 +0100

    test commit

--- src/com/cts/timesheet/model/TimeSheetUser.java
+++ src/com/cts/timesheet/model/TimeSheetUser.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//This is the test comment for GIT from web
+//This is the test comment for GIT from git
 @Entity
 @Table(name="TIMESHEET_USERS")
 public class TimeSheetUser implements User {
